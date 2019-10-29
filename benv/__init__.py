0664523e3428a26e71b0488354fd8407e0776846
Burak Yilmaz <burakyilmaz321@gmail.com>
2019-10-30 00:26:57 +0300

    Init python packaging

---
+++ benv/__init__.py
@@ -0,0 +1,11 @@
+def chenv(env_name):
+	print(f"Swithced to {env_name}")
+
+def mkenv(env_name):
+	print(f"Created environment {env_name}")
+
+def lsenv():
+	print("Here are some environments")
+
+def rmenv(env_name):
+	print(f"Removed environment {env_name}")
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='benv',
+	version='0.1',
+	description='A minimalistic Python virtual environment manager that no one needs',
+	url='https://github.com/burakyilmaz321/benv',
+	author='Burak Yilmaz',
+	author_email='burakyilmaz321@gmail.com',
+	license='MIT',
+	packages=['benv'],
+	zip_safe=False
+)

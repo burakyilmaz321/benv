ed6e1897cd8433477fa9d71a5e6b51d6f2e25b02
Burak Yilmaz <burakyilmaz321@gmail.com>
2019-11-05 00:38:48 +0300

    Fix flake8 and pylint errors

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
+"""Setup configuration"""
+
 from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    LONG_DESCRIPTION = fh.read()
 
 setup(
-	url='https://github.com/burakyilmaz321/benv',
-	license='MIT',
+    url='https://github.com/burakyilmaz321/benv',
+    license='MIT',
     name="benv-burakyilmaz321",
     version="0.1.1",
     author="Burak Yilmaz",
     author_email="burakyilmaz321@gmail.com",
-	description='A minimalistic Python virtual environment manager that no one needs',
-    long_description=long_description,
+    description='A minimalistic Python virtual environment manager'
+                'that no one needs',
+    long_description=LONG_DESCRIPTION,
+    install_requires=['click'],
     long_description_content_type="text/markdown",
     packages=find_packages(),
     classifiers=[
@@ -21,4 +25,8 @@ setup(
         "Development Status :: 1 - Planning",
     ],
     python_requires='>=3.3',
+    entry_points='''
+        [console_scripts]
+        benv=benv.benv:cli
+    ''',
 )

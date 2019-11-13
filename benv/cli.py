95b7cc20712ebd886ed5a58369d240e92663cf1f
Burak Yilmaz <burakyilmaz321@gmail.com>
2019-11-13 18:19:19 +0300

    Colorize active environment

--- benv/cli.py
+++ benv/cli.py
@@ -4,6 +4,10 @@ import os
 
 import click
 
+
+RED = "\033[1;31m"
+RESET = "\033[0;0m"
+
 try:
     BENV_HOME = os.environ["BENV_HOME"]
 except KeyError:
@@ -12,6 +16,8 @@ except KeyError:
         "Please create it like: "
         "$ export BENV_HOME=~/.envs"
     )
+
+VIRTUAL_ENV = os.environ.get("VIRTUAL_ENV")
 
 
 @click.group()
@@ -39,7 +45,10 @@ def lsenv():
     envdir = os.scandir(BENV_HOME)
     envs = filter(lambda entry: entry.is_dir(), envdir)
     for env in envs:
-        print("*", env.name)
+        if VIRTUAL_ENV and os.path.basename(VIRTUAL_ENV) == env.name:
+            print(f"{RED}* {env.name}{RESET}")
+        else:
+            print(f"* {env.name}")
 
 
 @cli.command()
